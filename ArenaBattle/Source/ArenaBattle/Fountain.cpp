67b9424141691957cc6693d9c1f96e054988744d
ba5909 <ba5909@naver.com>
2020-09-09 22:11:35 +0900

    Chapter2 액터의 설계
    액터를 C++로 직접 설계하는 예제

--- ArenaBattle/Source/ArenaBattle/Fountain.cpp
+++ ArenaBattle/Source/ArenaBattle/Fountain.cpp
@@ -9,6 +9,46 @@ AFountain::AFountain()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
+	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
+	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+	//생성자 코드에서 컴포넌트를 생성하는 용도로 언리얼 엔진은 new가 아닌 CreateDefaultSubobject API라는 함수를 사용한다.
+
+	//분수대 구조물에서 Body가 대표이므로 Water는 Body의 자식이 되도록 설정한다.
+	RootComponent = Body;
+	Water->SetupAttachment(Body);
+	Light->SetupAttachment(Body);
+	Splash->SetupAttachment(Body);
+
+	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 135.0f));	//컴포넌트의 기본 위치값 설정
+	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01"));
+	//애셋에 대한 오브젝트 경로 전달
+
+	if (SM_BODY.Succeeded()) {
+		Body->SetStaticMesh(SM_BODY.Object);	//애셋에 매시 지정하기
+	}
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_02.SM_Plains_Castle_Fountain_02"));
+	//애셋에 대한 오브젝트 경로 전달
+
+	if (SM_WATER.Succeeded()) {
+		Water->SetStaticMesh(SM_WATER.Object);	//애셋에 매시 지정하기
+	}
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem>
+		PS_SPLASH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01"));
+	//애셋에 대한 오브젝트 경로 전달
+
+	if (PS_SPLASH.Succeeded()) {
+		Splash->SetTemplate(PS_SPLASH.Object);	//애셋에 매시 지정하기
+	}
 }
 
 // Called when the game starts or when spawned
--- ArenaBattle/Source/ArenaBattle/Fountain.h
+++ ArenaBattle/Source/ArenaBattle/Fountain.h
@@ -2,14 +2,14 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"	//다양한 엔진 기능이 필요하기 때문에 CoreMinimal.h대신 사용한다.
 #include "GameFramework/Actor.h"
-#include "Fountain.generated.h"
+#include "Fountain.generated.h"	//언리얼 오브젝트 선언의 마지막 #include 구문에 이 헤더 파일을 반드시 선언해야 한다.
 
-UCLASS()
-class ARENABATTLE_API AFountain : public AActor
+UCLASS()	//클래스 선언 매크로: 해당 클래스가 언리얼 오브젝트임을 선언하기 위한 매크로
+class ARENABATTLE_API AFountain : public AActor	//접두사 A는 액터를 나타낸다. U는 액터가 아닌 클래스를 나타낸다.
 {
-	GENERATED_BODY()
+	GENERATED_BODY()	//클래스 내부 매크로
 	
 public:	
 	// Sets default values for this actor's properties
@@ -23,4 +23,28 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere)	//VisibleAnywhere는 디테일 윈도우에서 컴포넌트를 편집하기 위해.
+		/*메모리 관리용 언리얼 매크로. 
+		포인터로 선언했으므로 메모리를 동적으로 할당해 대입한 것이다. 
+		매크로를 이용해 메모리 자동 소멸 기능을 제공한다.
+		언리얼 오브젝트에서만 사용할 수 있는 기능이다.
+		잊지 말아야 한다.*/
+	UStaticMeshComponent* Body;		//새로 추가한 선언 구문
+
+	UPROPERTY(VisibleAnywhere) //메모리 관리용 언리얼 매크로
+	UStaticMeshComponent* Water;	//새로 추가한 선언 구문
+
+	UPROPERTY(VisibleAnywhere)
+		UPointLightComponent* Light;
+
+	UPROPERTY(VisibleAnywhere)
+		UParticleSystemComponent* Splash;
+
+	//UPROPERTY(VisibleAnywhere)
+	//	int32 ID;					//멤버 변수
+	//VisibleAnywhere는 디테일 윈도우에서 컴포넌트를 편집하기 위해 사용할 수 있지만 해당 객체를 다른 객체로 변경은 불가하다.
+	//ID는 0이며 0을 편집할 수 없다.
+
+	UPROPERTY(EditAnywhere, Category=ID)	//에디터에서 편집 가능한 키워드. Category=분류명
+		int32 ID;					//멤버 변수
 };
