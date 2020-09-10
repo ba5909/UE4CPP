49d17ddf8526e46df4469359dd1863158da2aa1f
ba5909 <ba5909@naver.com>
2020-09-10 23:19:14 +0900

    Chapter 3 움직이는 액터 제작
    액터에 회전값 넣기

--- ArenaBattle/Source/ArenaBattle/Fountain.h
+++ ArenaBattle/Source/ArenaBattle/Fountain.h
@@ -2,7 +2,9 @@
 
 #pragma once
 
-#include "EngineMinimal.h"	//다양한 엔진 기능이 필요하기 때문에 CoreMinimal.h대신 사용한다.
+//#include "EngineMinimal.h"	//다양한 엔진 기능이 필요하기 때문에 CoreMinimal.h대신 사용한다.
+#include "ArenaBattle.h"	//로깅을 위한 공용 매크로 설정에서
+#include "GameFramework/RotatingMovementComponent.h"
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"	//언리얼 오브젝트 선언의 마지막 #include 구문에 이 헤더 파일을 반드시 선언해야 한다.
 
@@ -10,7 +12,7 @@ UCLASS()	//클래스 선언 매크로: 해당 클래스가 언리얼 오브젝트임을 선언하기 위한 �
 class ARENABATTLE_API AFountain : public AActor	//접두사 A는 액터를 나타낸다. U는 액터가 아닌 클래스를 나타낸다.
 {
 	GENERATED_BODY()	//클래스 내부 매크로
-	
+
 public:	
 	// Sets default values for this actor's properties
 	AFountain();
@@ -18,6 +20,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void PostInitializeComponents() override;
 
 public:	
 	// Called every frame
@@ -47,4 +51,11 @@ public:
 
 	UPROPERTY(EditAnywhere, Category=ID)	//에디터에서 편집 가능한 키워드. Category=분류명
 		int32 ID;					//멤버 변수
+
+	UPROPERTY(VisibleAnywhere)
+		URotatingMovementComponent* Movement;	//지정한 속도로 액터를 회전시킨다
+
+private:
+	UPROPERTY(EditAnywhere, Category = Stat, Meta = (AllowPrivateAccess = true))
+		float RotateSpeed;
 };
